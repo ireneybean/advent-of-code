bf2eff52cda410b3732ebe7c3dad6d537e2f1077
Irene Shaw <irene.shaw@omnicell.com>
2019-12-08 15:13:58 -0500

    Add param type support and input/output (first pass)

--- 2019/5.rb
+++ 2019/5.rb
@@ -3,18 +3,4 @@ require './lib/intcode_program.rb'
 
 intcode = AdventOfCode::inputs(5).first.split(',').map!(&:to_i)
 
-class IntcodeProgramExtended < IntcodeProgram
-  
-  def do_opcode
-    case opcode
-    when 3
-      @hopsize = 2
-      store
-    when 4
-      @hopsize = 2
-      output
-    else 
-      super
-    end    
-  end
-end
+
--- 2019/lib/intcode_program.rb
+++ 2019/lib/intcode_program.rb
@@ -1,13 +1,14 @@
 module Intcode
   class Opcode
     attr_reader :size
+    
     def initialize(opcode)
-      @code = opcode
+      parse_instruction(opcode)
       case @code
-      when 1
+      when 1,2
         @size = 4
-      when 2
-        @size = 4
+      when 3,4  
+        @size = 2
       when 99
         @size = 1
       else
@@ -22,10 +23,23 @@ module Intcode
         program.add
       when 2
         program.multiply
+      when 3
+        user_input = 1
+        program.store(user_input)
+      when 4
+        program.output    
       when 99
         program.done = true
       end
     end  
+    
+    private 
+    
+    def parse_instruction(code)
+      full_code = "%05d" % code
+      @code = full_code[3,2].to_i
+      @param_modes = full_code[0, 3].split('').map(&:to_i).reverse
+    end
   end   
    
   class Program
@@ -47,31 +61,40 @@ module Intcode
   
     def do_next_instruction
       # puts "processing opcode at: #{position}: #{self.memory[self.position]}"
-      current_opcode = Opcode.new(self.memory[self.position])
-      current_opcode.run(self)
-      @hopsize = current_opcode.size
+      @current_opcode = Opcode.new(self.memory[self.position])
+      @current_opcode.run(self)
+
     end
   
     def add
-      self.memory[write_position] = val(1) + val(2)
+      self.memory[write_position] = val_at(1) + val_at(2)
     end
   
     def multiply
-      self.memory[write_position] = val(1) * val(2)
+      self.memory[write_position] = val_at(1) * val_at(2)
     end  
+    
+    def store(input)
+      self.memory[write_position] = input
+    end
+    
+    def output
+      puts "Output: #{self.memory[write_position]}"
+    end
+    
     private
   
-    def val(num)
+    def val_at(num)
       position = self.memory[self.position + num]
       self.memory[position]
     end
 
     def write_position
-      self.memory[self.position + 3]
+      self.memory[self.position + @current_opcode.size - 1]
     end 
     
     def advance
-      self.position = self.position + @hopsize
+      self.position = self.position + @current_opcode.size
     end  
            
   end
